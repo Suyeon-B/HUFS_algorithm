cfaad26acc6f2758bec55b6ebd2978ff4adcee6a
Suyeon-B <tndus4243@naver.com>
2021-05-16 07:37:56 +0900

    Update 왼쪽 정렬.py

--- 왼쪽 정렬.py
+++ 왼쪽 정렬.py
@@ -4,30 +4,62 @@ words = input().split()
 len_words = [None] * len(words)
 for i in range(len(words)):
 	len_words[i] = len(words[i])
+
 # 1) w 내의 값이라면 최대한 붙여 쓴 경우
 len_one_line = [0] * len(words)
 len_one_line[0] = len(words[0])
 penalties = [0] * len(words)
-space = 0
+
 i = 0
 j = 0
 while(True):
 	if i+1 == len(words):
 		break
-	if (len_one_line[j] + len_words[i+1] + 1 <= W):
+	if ((len_one_line[j] + len_words[i+1] + 1 <= W) and (len_words[i+1]>1)):
 		len_one_line[j] += 1 + len_words[i+1]
 		i+=1
 	else:
-		penalties[j] = (W - len_one_line[j])**3
-		j+=1
-		i+=1
-		len_one_line[j] = len_words[i]
-		penalties[j] = (W - len_one_line[j])**3
+		if (len_words[i+1]>1) or (i+2 == len(words)):
+			penalties[j] = (W - len_one_line[j])**3
+			j+=1
+			i+=1
+			len_one_line[j] = len_words[i]
+			penalties[j] = (W - len_one_line[j])**3
+		else:
+			j += 1
+			len_one_line[j] += 1 + len_words[i + 1] + len_words[i + 2]
+			penalties[j] = (W - len_one_line[j]) ** 3
+			i += 2
+			j += 1
+'''
+panalty = 0
+for i in range(len(penalties)):
+	if penalties[i] == 0:
+		break
+	panalty += penalties[i]
+
+# 2) 한 줄에 표시할 단어의 조합이 (앞+) (+뒤) 중 패널티 값이 작은 것으로 택할 경우
+len_one_line = [0] * len(words)
+len_one_line[0] = len(words[0])
+penalties = [0] * len(words)
+
+i = 0
+j = 0
+while(True):
+	if i+1 == len(words):
+		break
+	# (+뒤)의 패널티 값이 작은 경우
+	if (j == 0) or (len_one_line[j] + 1 + len_words[i+1] > len_one_line[j+1] + ):
+		len_one_line[j] += 1 + len_words[i+1]
+	# (앞+)의 패널티 값이 작은 경우
+
+'''
 
 panalty = 0
 for i in range(len(penalties)):
+	if penalties[i] == 0 and i != 0:
+		break
 	panalty += penalties[i]
-# 2)
 
 # 결과 : 최소 penalty값
 print(panalty)
