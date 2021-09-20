dfc0a27632c4672ab7ac0f07f7145d1c7b38a511
Suyeon-B <tndus4243@naver.com>
2021-09-21 02:05:40 +0900

    간단한 ver.

--- 골치 아픈 이사/골치 아픈 이사.py
+++ 골치 아픈 이사/골치 아픈 이사.py
@@ -4,23 +4,20 @@ num_of_houses = len(address) # 집의 개수
 
 
 # 번지수 차이를 계산해 저장한다.
-distance_sum = 0
-distance_sum_list = []
-
+distance = 0
 distances = []
+address.sort() # 리스트를 정렬해서 가운데 값을 찾는다.
+if num_of_houses%2 != 0: # 집이 홀수개 있다면
+    for i in range(num_of_houses):
+        distance += abs(address[num_of_houses//2] - address[i]) # 정 가운데 값이 거리의 합이 최소가 되는 집이다.
+    print(distance)
+else: # 짝수개 있다면 가운데 두 집들 중 하나가 거리의 합이 최소가 되는 집이다.
+    for i in range(num_of_houses):
+        distance += abs(address[num_of_houses//2] - address[i]) # 가운데의 왼쪽 집
+    distances.append(distance)
 
-for i in range(num_of_houses):
-    line = []
-    for j in range(num_of_houses):
-        if i<j:
-            line.append(abs(address[i] - address[j]))
-        elif i>j:
-            line.append(distances[j][i])
-        else:
-            line.append(0)
-    distances.append(line)
-    distance_sum = sum(distances[i])
-    distance_sum_list.append(distance_sum)
+    for i in range(num_of_houses):
+        distance += abs(address[num_of_houses//2+1] - address[i]) # 가운데의 오른쪽 집
+    distances.append(distance)
 
-
-print(min(distance_sum_list))
+    print(min(distances))
