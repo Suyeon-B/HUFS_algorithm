d9e2397d94bdcf1a647d05b822a85108aca8d665
Suyeon-B <tndus4243@naver.com>
2021-09-22 01:18:35 +0900

    아이디어 2 - 시간복잡도 문제 해결해야함

--- 배열+스캔 - 멀리 가볼까?/배열+스캔 - 멀리 가볼까?.py
+++ 배열+스캔 - 멀리 가볼까?/배열+스캔 - 멀리 가볼까?.py
@@ -1,32 +1,41 @@
-# 끝에서 0이 아닌 2번째 수를 기준으로 한다.
-# 우선 끝에서 0이 아닌 2번째 수에서 마지막까지 도달할 수 있는가를 판단해서,
-# 불가능하면 False, 가능하면 다음 단계를 확인한다.
-# 끝에서 0이 아닌 2번째 수로 도달할 수 있는 앞 인덱스의 값이 있는가를 판단한다. (인덱스 0이 될 때 까지 확인한다.)
+# 0을 기준으로 왼쪽 값들을 차례로 검사하여,
+# (모든 0에 대하여) 0의 왼쪽값들과 0의 인덱스 차이 < 해당 인덱스의 값 이면
+# Jump하여 끝까지 도달할 수 있고,
+# 반대의 경우라면 도달할 수 없다.
+
 def JUMP(A):
-    current_idx = -1
     size = len(A)
-    is_second = 0
-    # 끝에서 0이 아닌 2번째 수를 기준으로 한다.
-    for i in range(size-1, 0, -1):
-        if A[i] != 0:
-            is_second += 1
-            if is_second == 2:
-                current_idx = i
-                break
+    zero = 0
+    zero_idx = []
+    for i in range(size):
+        if A[i] == 0:
+            zero += 1
+            zero_idx.append(i)
+
+    temp = list(zero_idx)
+    for i in range(zero-1):
+        if temp[i]+1 == temp[i+1]:
+            zero_idx.pop(i)
+            zero -= 1
 
 
-    while True:
-        # 우선 끝에서 0이 아닌 2번째 수에서 마지막까지 도달할 수 있는가를 판단해서,
-        remain_behind_idx = size - current_idx -1
 
-        if A[current_idx] < remain_behind_idx: # 못간다면 False를 출력하고,
-            return False
-        else: # 갈 수 있다면 다시 앞쪽을 살핀다.
-            if A[current_idx-A[current_idx]] >= current_idx-A[current_idx]:
-                current_idx = current_idx-A[current_idx]
+    for i in range(zero):
+        count = zero_idx[i]
+        start = i
+        while True:
+            if A[start] <= zero_idx[i]-start:
+                if start+1 == size-1:
+                    return True
+                if count > 0 :
+                    count -= 1
+                    start += 1
+                    continue
+                else:
+                    return False
             else:
-                return False
-            # 안되는 수 앞에도 가능함을 고려해야함
+                break
+    return True
 
 
 
